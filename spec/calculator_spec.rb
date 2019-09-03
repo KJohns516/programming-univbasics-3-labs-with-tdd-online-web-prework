ff7cd5f216d5d0479b93e85aeb7d5b307a99b9a7
Kristan Johns <kristandjohns@gmail.com>
2019-09-03 00:45:54 +0000

    Automatically backed up by Learn

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,46 +1,6 @@
 # Code your solution in calculator.rb
 
 describe "./calculator.rb" do
-  
-def add(x,y)
-  x + y
-end
-
-def subtract(x,y)
-  x - y
-end
-
-def sum(a)
-  s = 0
-  a.each do |x|
-    s += x
-  end
-  s
-end
-
-def multiply(x,y)
-  x*y
-end
-
-def product(a)
-  p = 1
-  a.each do |x|
-    p *= x
-  end
-  p
-end
-
-def power(x,y)
-  x**y
-end
-
-def fact(n)
-  f = 1
-  (2..n).each do |k|
-    f *= k
-  end
-  f
-end
 
   it "contains a local variable called first_number that is assigned to a number" do
     first_number = get_variable_from_file('./calculator.rb', "first_number")
